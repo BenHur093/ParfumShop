e82e17efe8f2aba1f9b283293c15eee3e25cdaa2
BenHur093 <benkehunor04@gmail.com>
2025-05-04 22:32:42 +0200

    fooldal elkezdve

---
+++ script.js
@@ -0,0 +1,10 @@
+function openMenu() {
+    document.getElementById("side-menu").style.width = "300px";
+    
+}
+
+function closeMenu() {
+    document.getElementById("side-menu").style.width = "0";
+}
+
+
